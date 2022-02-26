5fb021aa09b3844b1136f6f0bbf05776b0ec9ecb
Christian <6939810+chkr1011@users.noreply.github.com>
2022-02-26 10:07:03 +0100

    Move files.

---
+++ Source/MQTTnetAppWebAssembly/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MQTTnetAppWebAssembly.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var x = CreateHostBuilder(args).Build();
+        
+        
+        
+        await x.RunAsync();
+    }
+
+    public static WebAssemblyHostBuilder CreateHostBuilder(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        
+        builder.RootComponents.Add<App>("#app");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        return builder;
+    }
+}
+
+
+
+
